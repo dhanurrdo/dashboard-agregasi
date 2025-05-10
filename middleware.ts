0128932685b525ab245dffe68a7a58b70fd81d04
Dhanur Ridho <34299157+dhanurrdo@users.noreply.github.com>
2025-05-10 10:38:52 +0700

    Updated middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,18 +2,18 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  // const isAuthenticated = request.cookies.has('auth_token')
-  // const isLoginPage = request.nextUrl.pathname === '/login'
+  const isAuthenticated = request.cookies.has('auth_token')
+  const isLoginPage = request.nextUrl.pathname === '/login'
 
-  // // If user is not authenticated and not on login page, redirect to login
-  // if (!isAuthenticated && !isLoginPage) {
-  //   return NextResponse.redirect(new URL('/login', request.url))
-  // }
+  // If user is not authenticated and not on login page, redirect to login
+  if (!isAuthenticated && !isLoginPage) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
 
-  // // If user is authenticated and tries to access login page, redirect to dashboard
-  // if (isAuthenticated && isLoginPage) {
-  //   return NextResponse.redirect(new URL('/dashboard', request.url))
-  // }
+  // If user is authenticated and tries to access login page, redirect to dashboard
+  if (isAuthenticated && isLoginPage) {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
 
   return NextResponse.next()
 }
